755e5e7d1534e30a64e781fca6220c2cecafb31a
Todd Nichols <nivram80@gmail.com>
2014-04-07 11:47:05 -0500

    add new player partial and ajax add player link to team page

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,12 @@
 class Player < ActiveRecord::Base
-  attr_accessible :bats, :dob, :email, :fname, :lname, :password_digest, :phone, :team_id, :throws
+  attr_accessible :bats, :dob, :email, :fname, :lname, :phone, :team_id, :throws, :password, :password_confirmation
+
+  has_secure_password
   belongs_to :team
 	has_many :positions
 	has_many :batter_game_stats
 	has_many :pitcher_game_stats
 	has_many :parents
+	
+	validates :email, :uniqueness => true
 end
