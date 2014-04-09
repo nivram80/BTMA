4783ff408e1e87f7becb73bb37b54b781ac47679
Todd Nichols <nivram80@gmail.com>
2014-04-08 20:34:48 -0500

    unsuccessful attempt to add ajax to Add Player link on game show view

--- app/controllers/batter_game_stats_controller.rb
+++ app/controllers/batter_game_stats_controller.rb
@@ -1,32 +1,43 @@
 class BatterGameStatsController < ApplicationController
 
 	def index
-		@batter_game_stats = Batter_game_stat.all
+		@batter_game_stats = BatterGameStat.all
 	end
 
 	def show
-		@batter_game_stat = Batter_game_stat.find(params[:id])
+		@batter_game_stat = BatterGameStat.find(params[:id])
 	end
 
 	def new
-		@batter_game_stat = Batter_game_stat.new
+		@batter_game_stat = BatterGameStat.new
+		@batter_num = params[:batter_num]
+
+		respond_to do |format|
+      format.html
+      format.js 
+    end
 	end
 
 	def create
-		@batter_game_stat = Batter_game_stat.create(params[:batter_game_stat])
+		@batter_game_stat = BatterGameStat.create(params[:batter_game_stat])
+
+		respond_to do |format|
+      format.html
+      format.js 
+    end
 	end
 
 	def edit
-		@batter_game_stat = Batter_game_stat.find(params[:id])
+		@batter_game_stat = BatterGameStat.find(params[:id])
 	end
 
 	def update
-		@batter_game_stat = Batter_game_stat.find(params[:id])
+		@batter_game_stat = BatterGameStat.find(params[:id])
 		@batter_game_stat.update_attributes(params[:batter_game_stat])
 	end
 
 	def destroy
-		@batter_game_stat = Batter_game_stat.find(params[:id])
+		@batter_game_stat = BatterGameStat.find(params[:id])
 	end
 
 
--- app/models/batter_game_stat.rb
+++ app/models/batter_game_stat.rb
@@ -1,5 +1,5 @@
 class BatterGameStat < ActiveRecord::Base
-  attr_accessible :ab, :bb, :cs, :double, :game_id, :hb, :hits, :hr, :ibb, :order_in_lineup, :player_id, :position, :rbi, :runs, :sb, :so, :triple
+  attr_accessible :ab, :bb, :cs, :double, :game_id, :hb, :hits, :hr, :ibb, :order_in_lineup, :player_id, :rbi, :runs, :sb, :so, :triple, :position_id
   belongs_to :player
 	belongs_to :game
 end
