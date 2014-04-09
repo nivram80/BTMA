3a97b4d6f32257867e1338eaf958a3dfb99accd1
Todd Nichols <nivram80@gmail.com>
2014-04-09 15:05:24 -0500

    add odd row table color and hover bg color

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 BatterGameStat.create({:game_id => 4, :player_id => 11, :order_in_lineup => 1, :ab => 4, :hits => 3, :runs => 3, :double => 0, :triple => 0, :hr => 0, :rbi => 1, :sb => 3, :position_id => 9})
+PitcherGameStat.create({:game_id = })
