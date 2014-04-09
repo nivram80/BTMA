261046112c85b07320fbff047cb1711c82631a92
Todd Nichols <nivram80@gmail.com>
2014-04-09 14:38:10 -0500

    add batter stats to team page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,7 +6,7 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find(params[:id])
     session[:team_id] = params[:id]
-    @players = Player.where(team_id: params[:id])
+    @players = Player.where(team_id: params[:id]).order(:lname)
     @coaches = Coach.where(team_id: params[:id])
   end
 
