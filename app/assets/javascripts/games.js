4e656ec97ff6ab917c62de7babcaf9b0c11c09ee
Todd Nichols <nivram80@gmail.com>
2014-04-16 09:16:43 -0500

    hide add batter/pitcher link on game page

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('.add_pitcher_stats').hide();
   $('.add_pitcher_link').click(function() {
   	$('.add_pitcher_stats').show();
+  	$('.add_pitcher_link').hide();
   });
 });
 
@@ -9,5 +10,6 @@ $(document).ready(function() {
   $('.add_batter_stats').hide();
   $('.add_batter_link').click(function() {
   	$('.add_batter_stats').show();
+  	$('.add_batter_link').hide();
   });
 });
