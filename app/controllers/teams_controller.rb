d2f90126d722d99079deeaad387cd6db9cffad36
Todd Nichols <nivram80@gmail.com>
2014-04-17 17:12:25 -0500

    attempt to retrieve all positions for one player - sort of fail

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,29 +7,33 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:id])
     @coaches = Coach.where(team_id: params[:id])
 
-    @players = Player.select("id, fname, lname, bats, throws, team_id")
-      .group("id")
-      .order(:lname)
-      .where("team_id = ?", session[:team_id])
+    @position_players = BatterGameStat.select("player_id, players.fname AS fname, players.lname AS lname, players.bats AS bats, players.throws AS throws, position_id, positions.position AS position").group("batter_game_stats.player_id, batter_game_stats.position_id, fname, lname, bats, throws, position").joins(:player, :position).order(:lname)
 
-    @pitchers = PitcherGameStat.select("
-      SUM(ip) AS sum_ip, 
-      SUM(er) AS sum_er, 
-      SUM(bb) AS sum_bb, 
-      SUM(hits) AS sum_hits, 
-      SUM(so) AS sum_so, 
-      SUM(win) AS sum_win, 
-      SUM(loss) AS sum_loss, 
-      SUM(sv) AS sum_sv, 
-      player_id, 
-      players.fname AS fname, 
-      players.lname AS lname, 
-      players.throws AS throws,
-      players.team_id AS team_id")
-      .group("pitcher_game_stats.player_id, fname, lname, throws, team_id")
-      .joins(:player)
-      .order(:lname)
-      .where("players.team_id = ?", session[:team_id])
+    # @position_players = Player.where("team_id = ?", 1).joins(:batter_game_stats => :position).pluck("positions.position AS pos").uniq.join(", ")
+
+    # @position_players = Player.select("id, fname, lname, bats, throws, team_id")
+    #   .group("id")
+    #   .order(:lname)
+    #   .where("team_id = ?", session[:team_id])
+
+    # @pitchers = PitcherGameStat.select("
+    #   SUM(ip) AS sum_ip, 
+    #   SUM(er) AS sum_er, 
+    #   SUM(bb) AS sum_bb, 
+    #   SUM(hits) AS sum_hits, 
+    #   SUM(so) AS sum_so, 
+    #   SUM(win) AS sum_win, 
+    #   SUM(loss) AS sum_loss, 
+    #   SUM(sv) AS sum_sv, 
+    #   player_id, 
+    #   players.fname AS fname, 
+    #   players.lname AS lname, 
+    #   players.throws AS throws,
+    #   players.team_id AS team_id")
+    #   .group("pitcher_game_stats.player_id, fname, lname, throws, team_id")
+    #   .joins(:player)
+    #   .order(:lname)
+    #   .where("players.team_id = ?", session[:team_id])
   end
 
   def new
