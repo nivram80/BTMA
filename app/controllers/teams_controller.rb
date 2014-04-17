63755fa73a04575e822416ae9bf18c51b8ac114f
Todd Nichols <nivram80@gmail.com>
2014-04-16 22:02:03 -0500

    add join to homepage queries - need to figure out the batter position column

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,8 +5,39 @@ class TeamsController < ApplicationController
 
   def show
     @team = Team.find(params[:id])
-    @players = Player.where(team_id: params[:id]).order(:lname)
     @coaches = Coach.where(team_id: params[:id])
+
+    @position_players = BatterGameStat.select("
+      SUM(ab) AS sum_ab,
+      SUM(hits) AS sum_hits, 
+      SUM(hr) AS sum_hr, 
+      SUM(rbi) AS sum_rbi, 
+      SUM(sb) AS sum_sb, 
+      player_id, 
+      players.fname AS fname,
+      players.lname AS lname, 
+      players.bats AS bats, 
+      players.throws AS throws")
+      .group("batter_game_stats.player_id, fname, lname, bats, throws")
+      .joins(:player, :position)
+      .order(:lname)
+
+    @pitchers = PitcherGameStat.select("
+      SUM(ip) AS sum_ip, 
+      SUM(er) AS sum_er, 
+      SUM(bb) AS sum_bb, 
+      SUM(hits) AS sum_hits, 
+      SUM(so) AS sum_so, 
+      SUM(win) AS sum_win, 
+      SUM(loss) AS sum_loss, 
+      SUM(sv) AS sum_sv, 
+      player_id, 
+      players.fname AS fname, 
+      players.lname AS lname, 
+      players.throws AS throws")
+      .group("pitcher_game_stats.player_id, fname, lname, throws")
+      .joins(:player)
+      .order(:lname)
   end
 
   def new
