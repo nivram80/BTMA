75736bc6f715af5540d94ca6f19d66e2663a360d
Todd Nichols <nivram80@gmail.com>
2014-04-18 12:44:40 -0500

    add Nevermind link to create team form

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
 
+  // Login page new team
+  $('.no_add_team').hide();
 	$('.new_team').hide();
 	$('.new_team_link').click(function() {
   	$('.login_container').hide();
   	$('.new_team').show();
   	$('.new_team_link').hide();
   	$('.new_team_link_head').hide();
+    $('.no_add_team').show();
   });
 
+  $('.no_add_team').click(function() {
+    $('.login_container').show();
+    $('.new_team').hide();
+    $('.new_team_link').show();
+    $('.new_team_link_head').show();
+    $('.no_add_team').hide();
+  });
+
+  // Team page new player
 	$('.no_add_player').hide();
 	$('.new_player').hide();
 	$('.new_player_link').click(function() {
@@ -22,6 +34,7 @@ $(document).ready(function() {
   	$('.no_add_player').hide();
   });
 
+  // Team page new coach
 	$('.no_add_coach').hide();
   $('.new_coach').hide();
 	$('.new_coach_link').click(function() {
