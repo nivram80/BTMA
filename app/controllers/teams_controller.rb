71856487e0c21f35d701ce862fa8d2885232ab75
Todd Nichols <nivram80@gmail.com>
2014-04-18 23:58:08 -0500

    refactor activerecord query for player info on homepage

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -9,14 +9,15 @@ class TeamsController < ApplicationController
     @coaches = Coach.where(team_id: params[:id])
     @player = Player.new
 
-    @position_players = BatterGameStat.select("player_id, players.fname AS fname, players.lname AS lname, players.bats AS bats, players.throws AS throws, position_id, positions.position AS position").group("batter_game_stats.player_id, batter_game_stats.position_id, fname, lname, bats, throws, position").joins(:player, :position).order(:lname)
+    @position_players = BatterGameStat
+      .select("player_id, players.fname || ' ' || players.lname AS name, players.bats AS bats, players.throws AS throws, array_agg(distinct positions.position) AS pos")
+      .group("player_id, fname, lname, throws, bats")
+      .joins(:player, :position)
+      .order(:lname)
 
-    # @position_players = Player.where("team_id = ?", 1).joins(:batter_game_stats => :position).pluck("positions.position AS pos").uniq.join(", ")
+    # @position_players = BatterGameStat.select("player_id, players.fname || ' ' || players.lname AS name, players.bats AS bats, players.throws AS throws, position_id, array_agg(positions.position) AS pos").group("batter_game_stats.player_id, batter_game_stats.position_id, fname, lname, bats, throws, position").joins(:player, :position).order(:lname)
 
-    # @position_players = Player.select("id, fname, lname, bats, throws, team_id")
-    #   .group("id")
-    #   .order(:lname)
-    #   .where("team_id = ?", session[:team_id])
+    # @position_players = Player.where("team_id = ?", session[:team_id]).joins(:batter_game_stats => :position).pluck("positions.position AS pos").uniq.join(", ")
 
   end
 
