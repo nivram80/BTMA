8a366618661f4a6d8d4c9111ca6844ae92c5e6d2
Todd Nichols <nivram80@gmail.com>
2014-05-06 01:40:17 -0500

    fix player team_id on staging

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,12 +6,11 @@ class GamesController < ApplicationController
 
 	def show
 		@game = Game.find(params[:id])
-		# binding.pry
 		@batter_stats = BatterGameStat.where(:game_id => params[:id]).order(:order_in_lineup)
 		@pitcher_stats = PitcherGameStat.where(:game_id => params[:id]).order(:pitcher_num)
 		@pitcher_game_stat = PitcherGameStat.new
 		@batter_game_stat = BatterGameStat.new
-		@players = Player.where("team_id = ?", params[:id])
+		@players = Player.where("team_id = ?", @game.team_id)
 		@positions = Position.all
 	end
 
